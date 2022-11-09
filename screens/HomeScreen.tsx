de1b884eae79f0ba35347da02f457a516ef00cde
Menake <menake.j@jgsw.global>
2022-11-09 20:36:24 +0530

    fix

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,23 @@
-import { View, Text } from "react-native";
+import { doc, setDoc } from "firebase/firestore";
+import { View, Text, Button } from "react-native";
 import { useTailwind } from "tailwind-rn";
-
+import { db } from "../firebaseConfig";
 
 const HomeScreen = () => {
   const tailwind = useTailwind();
 
+  const first = async () => {
+    await setDoc(doc(db, "cities", "LA"), {
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA",
+    });
+  };
+
   return (
     <View>
-      <Text style={tailwind("py-2 px-2")}>HomeScreen</Text>
+      <Text style={tailwind("py-4 px-2")}>HomeScreen</Text>
+      <Button onPress={first} title="test" />
     </View>
   );
 };
