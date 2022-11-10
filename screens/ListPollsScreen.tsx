09c2fac2377c7607af2ff3014780b8f826867534
Navindu <navanavi3@gmail.com>
2022-11-10 23:42:04 +0530

    feat: view poll, caast votes

--- screens/ListPollsScreen.tsx
+++ screens/ListPollsScreen.tsx
@@ -7,14 +7,17 @@ import {
   where,
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { View, Text, ScrollView, FlatList } from "react-native";
+import { View, Text, ScrollView, FlatList, StyleSheet } from "react-native";
 import { useTailwind } from "tailwind-rn";
 import { db } from "../firebaseConfig";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 const ListPollsScreen = ({ navigation }: any) => {
   const Item = ({ item }: any) => (
     <View style={tailwind("w-full flex flex-row bg-gray-200 rounded-md mb-4")}>
-      <View style={tailwind("px-2 py-2 ")}>
+      <View style={tailwind("px-2 py-2 flex justify-center")}>
         <View
           style={tailwind(
             "w-8 flex items-center justify-center bg-gray-300 py-1 rounded-sm"
@@ -24,8 +27,9 @@ const ListPollsScreen = ({ navigation }: any) => {
         </View>
       </View>
 
-      <View style={tailwind("flex-grow p-2")}>
+      <View style={tailwind("flex-grow p-2 flex justify-center")}>
         <Text
+          style={{ maxWidth: 260 }}
           textBreakStrategy="balanced"
           onPress={() => navigation.navigate("ViewPoll", { item })}
         >
@@ -34,8 +38,13 @@ const ListPollsScreen = ({ navigation }: any) => {
       </View>
 
       <View style={tailwind("p-3 flex items-center justify-center")}>
-        <Text style={{ opacity: 0.5 }} textBreakStrategy="balanced">
-          3m
+        <Text
+          style={{ opacity: 0.5, width: 50, fontSize: 12 }}
+          textBreakStrategy="balanced"
+        >
+          {`${
+            dayjs().isBefore(dayjs(item.endsOn.toDate())) ? "Ends" : "Ended"
+          } ${dayjs(item.endsOn.toDate()).fromNow()}`}
         </Text>
       </View>
     </View>
@@ -77,14 +86,33 @@ const ListPollsScreen = ({ navigation }: any) => {
   return (
     <View style={tailwind("p-4")}>
       <Text
-        style={tailwind("py-4")}
+        style={tailwind("py-4 font-primary-600 text-xl")}
         onPress={() => navigation.navigate("Create Poll")}
       >
-        Recent polls
+        Ongoing polls
       </Text>
 
       <FlatList
-        data={data}
+        data={data.filter((poll) =>
+          dayjs().isBefore(dayjs(poll.endsOn.toDate()))
+        )}
+        renderItem={Item}
+        onRefresh={getData}
+        refreshing={refreshing}
+        keyExtractor={(item) => item.id}
+      />
+
+      <Text
+        style={tailwind("py-4 font-primary-600 text-xl")}
+        onPress={() => navigation.navigate("Create Poll")}
+      >
+        Ended Polls
+      </Text>
+
+      <FlatList
+        data={data.filter((poll) =>
+          dayjs().isAfter(dayjs(poll.endsOn.toDate()))
+        )}
         renderItem={Item}
         onRefresh={getData}
         refreshing={refreshing}
--- screens/ViewPollScreen.tsx
+++ screens/ViewPollScreen.tsx
@@ -1,24 +1,91 @@
-import React from "react";
-import { View, Text, ScrollView, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import { useTailwind } from "tailwind-rn";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebaseConfig";
+dayjs.extend(relativeTime);
 
 const ViewPollScreen = ({ navigation, route }: any) => {
-  const tailwind = useTailwind();
+  // TODO: Replace with actual ID
+  const USER_ID = "7JwLj0rwO1uIkBg5lBZi";
 
-  const { id } = route.params;
+  const tailwind = useTailwind();
+  const { item } = route.params;
+
+  const [options, setOptions] = useState<any[]>([]);
+  const [responses, setResponses] = useState<any[]>([]);
+  const [voted, setVoted] = useState(false);
+
+  useEffect(() => {
+    getOptions();
+    getResponses();
+
+    // responses
+  }, []);
+
+  const getOptions = () => {
+    const ref = collection(db, "poll-options");
+    const q = query(ref, where("pollId", "==", item.id));
+
+    let data: any[] = [];
+    getDocs(q).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+      setOptions(data);
+    });
+  };
+
+  const getResponses = () => {
+    const ref = collection(db, "poll-responses");
+    const q = query(ref, where("pollId", "==", item.id));
+
+    let data: any[] = [];
+    getDocs(q).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        if (doc.data().userId == USER_ID) setVoted(true);
+        data.push({ id: doc.id, ...doc.data() });
+      });
+      setResponses(data);
+    });
+  };
+
+  const castVote = async (optId: any) => {
+    let ref = doc(collection(db, "poll-responses"));
+    let id = ref.id;
+
+    setVoted(true);
+    const data = {
+      userId: USER_ID,
+      pollId: item.id,
+      optionId: optId,
+    };
+
+    await setDoc(ref, data);
+  };
 
   return (
     <View style={tailwind("p-4")}>
-      <Text style={tailwind("py-4")}>View {id}</Text>
+      <Text style={tailwind("py-4")}>View</Text>
 
-      <Text style={tailwind("text-4xl font-primary-600")}>Yada yada yada</Text>
+      <Text style={tailwind("text-4xl font-primary-600")}>{item.topic}</Text>
 
-      <Text style={tailwind("mt-4")}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </Text>
+      <Text style={tailwind("mt-4")}>{item.description}</Text>
 
       <View
         style={tailwind(
@@ -26,20 +93,48 @@ const ViewPollScreen = ({ navigation, route }: any) => {
         )}
       >
         <Text style={tailwind("font-primary-600")}>By Username</Text>
-        <Text style={tailwind("font-primary-600")}>2 days ago</Text>
+        <Text style={tailwind("font-primary-600")}>{`Ends ${dayjs(
+          item.endsOn.toDate()
+        ).fromNow()}`}</Text>
       </View>
 
-      <View style={tailwind("mt-8")}>
-        <Button title="Yes" color="#000" />
-      </View>
+      <Text style={tailwind("text-xl mt-12 font-primary-600")}>
+        Cast your vote
+      </Text>
 
-      <View style={tailwind("mt-2")}>
-        <Button title="No" color="#000" />
-      </View>
-
-      <View style={tailwind("mt-2")}>
-        <Button title="Unsure" color="#000" />
-      </View>
+      {voted ? (
+        <View
+          style={{
+            width: "100%",
+            padding: 20,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#E7E7E7",
+            marginTop: 12,
+          }}
+        >
+          <Text>You have already voted</Text>
+        </View>
+      ) : dayjs().isBefore(dayjs(item.endsOn.toDate())) ? (
+        <View style={tailwind("mt-4")}>
+          {options && responses ? (
+            options.map((op) => (
+              <View key={op.id} style={tailwind("mt-2")}>
+                <Button
+                  onPress={() => castVote(op.id)}
+                  title={op.value}
+                  color="#000"
+                />
+              </View>
+            ))
+          ) : (
+            <ActivityIndicator />
+          )}
+        </View>
+      ) : (
+        <Text>Fucking TODO</Text>
+      )}
     </View>
   );
 };
