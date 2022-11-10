3a9d5943f0734c40572b4c1592c2eeab03beebc5
unknown <shapu953@gmail.com>
2022-11-10 16:51:01 +0530

    modify: user dashboard functionality

--- screens/UserDashboardScreen.tsx
+++ screens/UserDashboardScreen.tsx
@@ -10,7 +10,8 @@ import QuestionCard from "../components/QuestionCard";
 import DashboardInfo from "../components/DashboardInfo";
 
 const UserDashboardScreen = () => {
-    const [answers, setAnswers] = useState<any[]>();
+    const [answeredQuestions, setAnsweredQuestions] = useState<QuestionType[]>();
+     const [unansweredQuestions, setUnAnsweredQuestions] = useState<QuestionType[]>();
     const [questions, setQuestions] = useState<QuestionType[]>();
     const [limit, setLimit] = useState<number>(3)
 
@@ -25,7 +26,29 @@ const UserDashboardScreen = () => {
       });
       setQuestions(data);
     }
+    const getAnsweredQuestionData = async () => {
+    const ref = collection(db, "questions");
+    const q = query(ref, where("createdBy", "==", "7JwLj0rwO1uIkBg5lBZi"), where("isAnswered", "==", true))
+    let data: QuestionType[] = [];
+    const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() } as QuestionType);
+      });
+      setAnsweredQuestions(data);
+    }
+    const getUnAnsweredQuestionData = async () => {
+    const ref = collection(db, "questions");
+    const q = query(ref, where("createdBy", "==", "7JwLj0rwO1uIkBg5lBZi"), where("isAnswered", "==", false))
+    let data: QuestionType[] = [];
+    const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() } as QuestionType);
+      });
+      setUnAnsweredQuestions(data);
+    }
     getQuestionData()
+    getAnsweredQuestionData()
+    getUnAnsweredQuestionData()
     
   }, []);
 
@@ -36,9 +59,9 @@ const UserDashboardScreen = () => {
             <DashboardInfo firstname="Saul" lastname="Goodman" role="User"/>
             <View style={tailwind("mt-10 flex flex-row justify-evenly")}>
                 {questions && questions?.length ?  <DataCard type="Questions" number={questions?.length} p={`p-6`}/> : <DataCard type="Questions" number={0} p={`p-6`}/>}
+               {answeredQuestions && answeredQuestions?.length ? <DataCard type="Answered" number={answeredQuestions?.length} p={`p-6`}/> : <DataCard type="Answered" number={0} p={`p-6`}/>}
+                {unansweredQuestions && unansweredQuestions?.length ?  <DataCard type="Unanswered" number={unansweredQuestions?.length} p={`p-6`}/> :  <DataCard type="Unanswered" number={0} p={`p-6`}/>}
                
-                <DataCard type="Answered" number={23} p={`p-6`}/>
-                <DataCard type="Unanswered" number={23} p={`p-6`}/>
             </View>
             <View style={tailwind("mt-14")}>
                 <View>
